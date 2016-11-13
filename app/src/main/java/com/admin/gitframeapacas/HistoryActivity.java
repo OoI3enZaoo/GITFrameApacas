dbd62d55e93fd3a77be85d593d27767ec94f4517
OoI3enZaoo <Teerapath-Ben@hotmail.com>
2016-11-14 00:24:14 +0700

    Update RecyclerView

--- app/src/main/java/com/admin/gitframeapacas/HistoryActivity.java
+++ app/src/main/java/com/admin/gitframeapacas/HistoryActivity.java
@@ -3,19 +3,81 @@ package com.admin.gitframeapacas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * Created by Admin on 13/11/2559.
  */
 
-public class HistoryActivity extends DialogActivity {
+public class HistoryActivity extends AppCompatActivity {
+
+    RecyclerView recyclerview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        recyclerview = (RecyclerView) findViewById(R.id.historyRecyclerView);
+        //recyclerview.setHasFixedSize(true);
+        recyclerview.setAdapter(new RecyclrViewAdapter());
+        recyclerview.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        //recyclerview.setItemAnimator(new DefaultItemAnimator());
 
+        Log.d("ben","1");
+    }
+
+    public class RecyclrViewAdapter extends  RecyclerView.Adapter<ViewHolder>{
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            Log.d("ben","2");
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_histoy,parent,false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            holder.location.setText("ตลาดไทย");
+            holder.status.setText("อากาศเยี่ยมยอด");
+            Log.d("ben","3");
+        }
+        @Override
+        public int getItemCount() {
+            Log.d("ben","4");
+            return 10;
+        }
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView location;
+        TextView status;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            Log.d("ben","5");
+            location = (TextView) itemView.findViewById(R.id.location);
+            status = (TextView) itemView.findViewById(R.id.status);
+        }
+
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            Log.d("click","Click: " + position);
+        }
     }
 }
